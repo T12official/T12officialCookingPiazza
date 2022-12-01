3315490b5d46500fe4b83000e641ca622d532c8d
NeloBlivion <41271523+neloblivion@users.noreply.github.com>
2022-12-01 15:29:39 +0000

    Example Texture Draw

--- core/src/com/mygdx/game/T12Piazza.java
+++ core/src/com/mygdx/game/T12Piazza.java
@@ -4,24 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class T12Piazza extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture chef_1;
 	Texture chef_2;
+	TextureRegion chef_1_default;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		chef_1 = new Texture("chef/a2.png");
 		chef_2 = new Texture("chef/b2.png");
+		chef_1_default = new TextureRegion(chef_1, 34, 12, 30, 40);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(chef_1_default, 400, 200);
 		batch.end();
 	}
 
