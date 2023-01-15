0ff39070cccf3e0ce059131c5f4779f5e2095994
Ben31222 <br894@york.ac.uk>
2023-01-15 18:59:18 +0000

    15/01 update

--- core/src/com/mygdx/game/Level.java
+++ core/src/com/mygdx/game/Level.java
@@ -1,38 +1,79 @@
 package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.TimeUtils;
+import sun.tools.jconsole.JConsole;
+
 import java.util.*;
 public class Level {
     ArrayList<Customer> customerList = new ArrayList<>();
     ArrayList<Chef> chefList = new ArrayList<>();
     long timer;
     long idleTime;
-    int[][] gridArray;
+    List<List<String>> gridArray;
     String gameMode;
+    //TimeUtils.millis();
+    long timeToNextCustomer;
+    long getTimeToIdleGame;
 
-    private void updateTimer(long timePassed){
-        timer += timePassed;
-        idleTime += timePassed;
-        if (timer > 1000 ){	/* 1000 needs to be replaced by constant for how long between customers*/
-            nextCustomer();
-            timer = 0;
+
+    public long getTimeToNextCustomer(){
+        return timeToNextCustomer;
+    }
+
+
+    public void readAssetFile(String gameFile){
+
+        //This function reads read in a gameMap file and populates the gridArray variable which
+        //The gridArray is a 2d array that contains a map of the game world
+
+
+
+        FileHandle handle =  Gdx.files.local(gameFile);
+        String text = handle.readString();
+        //System.out.println(text);
+        String[] myStrings = text.split("\n");
+        for (int i = 0 ; i < myStrings.length ; i ++){
+            for (int x = 0 ; x < myStrings.length ; x ++){
+                if (x == 0){gridArray.add(new ArrayList<String>());}
+                gridArray.get(i).add(String.valueOf(myStrings[i].charAt(x)));
+            }
+
+
         }
-        if (idleTime > 1000){
-            gameMode = "idle";
-        }
+        System.out.println( gridArray.toString());
 
     }
-    private void initialiseLevel(){
-        timer = 0;
-        gridArray = new int[1][1];
+
+
+
+    public void initialiseLevel(){
+
+        timeToNextCustomer = 10000;
+        getTimeToIdleGame = 15000;
+
+        timer = TimeUtils.millis();
+        idleTime = TimeUtils.millis();
+        gridArray = new ArrayList<>();
+        readAssetFile("gameMaps/gameMap.txt");
     }
-    private void nextCustomer(){
+
+    public long getTimeElapsedMilliSeconds(){
+        return TimeUtils.timeSinceMillis(timer);
+    }
+    public long getTimeSinceLastIO(){
+        return TimeUtils.timeSinceMillis(idleTime);
+    }
+    public long getMinIdleTime(){
+        return getTimeToIdleGame;
+
+    }
+    public void nextCustomer(){
         if (customerList.size() > 0){
             customerList.get(customerList.size() - 1);
             customerList.remove(customerList.size() - 1);
+            timer = TimeUtils.millis();
         }
-
     }
-
-
-
 
 }
--- core/src/com/mygdx/game/T12Piazza.java
+++ core/src/com/mygdx/game/T12Piazza.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 import java.util.*;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,16 +15,33 @@ public class T12Piazza extends ApplicationAdapter {
 	Texture chef_2;
 	TextureRegion chef_1_default;
 
+	Level myLevelManager;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		chef_1 = new Texture("chef/a2.png");
 		chef_2 = new Texture("chef/b2.png");
 		chef_1_default = new TextureRegion(chef_1, 34, 12, 30, 40);
+		myLevelManager = new Level();
+		myLevelManager.initialiseLevel();
+
+
 	}
 
 	@Override
 	public void render () {
+
+		if (myLevelManager.getTimeToNextCustomer() < myLevelManager.getTimeElapsedMilliSeconds()) {
+			//This checks whether enough time has passed in order to spawn a new customer
+			//System.out.println("spawn Next Customer");
+			myLevelManager.nextCustomer();
+		}
+		if (myLevelManager.getTimeSinceLastIO() > myLevelManager.getMinIdleTime()){
+			//This is statement is checking the time since the last user I/O. If the time has been suffiently long it request the game move to the idle mode
+			//System.out.println("Activate idle");
+		}
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(chef_1_default, 400, 200);
