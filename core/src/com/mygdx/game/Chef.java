81d0839d67158f5f6d40587fdf8494d10c16cb85
Billy Brudenell <bb1085@york.ac.uk>
2023-01-18 18:13:26 +0000

    Added chef walking

--- core/src/com/mygdx/game/Chef.java
+++ core/src/com/mygdx/game/Chef.java
@@ -1,6 +1,85 @@
 package com.mygdx.game;
-import java.util.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Chef {
+public class Chef extends Sprite {
+    private static final int FRAME_COLS = 6;
+    private static final int FRAME_ROWS = 5;
+
+    SpriteBatch spriteBatch;
+    private final float stateTime;
+
+    public enum State {WALKING, STANDING};
+    State currentState;
+    String prevState;
+
+    private Texture texture;
+    TextureRegion chefStanding;
+    Animation<TextureRegion> playerWalk;
+    private Level level;
+    private float walkingSpeed;
+    private float runningSpeed;
+
+
+    public Chef(Level level){
+        texture = new Texture(Gdx.files.internal("chef/a1.png"));
+        walkingSpeed = 10;
+        TextureRegion[][] tmp = TextureRegion.split(texture,
+                texture.getWidth() / FRAME_COLS,
+                texture.getHeight() / FRAME_ROWS);
+
+        chefStanding = tmp[0][0];
+        TextureRegion[] frames = new TextureRegion[FRAME_COLS];
+        int index = 0;
+        for (int i = 0; i < FRAME_COLS; i++) {
+                frames[index++] = tmp[1][i];
+        }
+        currentState = State.WALKING;
+        playerWalk = new Animation<TextureRegion>(0.25f, frames);
+        stateTime = 0f;
+        spriteBatch = new SpriteBatch();
+    }
+
+    public void handleInput(){
+        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            this.setY(this.getY() + walkingSpeed);
+            currentState = State.WALKING;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            this.setY(this.getY() - walkingSpeed);
+            currentState = State.WALKING;
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            this.setX(this.getX() + walkingSpeed);
+            currentState = State.WALKING;
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            this.setX(this.getX() - walkingSpeed);
+            currentState = State.WALKING;
+        }
+        else currentState = State.STANDING;
+
+    }
+
+    public boolean isStanding(){
+        if (currentState == State.STANDING){
+            return true;
+        }
+        return false;
+    }
+
+    public void dispose (){
+        spriteBatch.dispose();
+        texture.dispose();
+    }
+
+
+
+
     
 }
--- core/src/com/mygdx/game/T12Piazza.java
+++ core/src/com/mygdx/game/T12Piazza.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 import java.util.*;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -9,24 +10,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class T12Piazza extends ApplicationAdapter {
+
+public class T12Piazza implements ApplicationListener {
 	SpriteBatch batch;
-	Texture chef_1;
+	Chef chef_1;
 	Texture chef_2;
 	TextureRegion chef_1_default;
 
 	Level myLevelManager;
+	float stateTime;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		chef_1 = new Texture("chef/a2.png");
 		chef_2 = new Texture("chef/b2.png");
-		chef_1_default = new TextureRegion(chef_1, 34, 12, 30, 40);
+		//chef_1_default = new TextureRegion(chef_1, 34, 12, 30, 40);
 		myLevelManager = new Level();
+		chef_1 = new Chef(myLevelManager);
 		myLevelManager.initialiseLevel();
+	}
 
+	@Override
+	public void resize(int width, int height) {
 
 	}
 
@@ -42,10 +48,28 @@ public class T12Piazza extends ApplicationAdapter {
 			//This is statement is checking the time since the last user I/O. If the time has been suffiently long it request the game move to the idle mode
 			//System.out.println("Activate idle");
 		}
+
+		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+
 		ScreenUtils.clear(1, 0, 0, 1);
+		TextureRegion chefFrame = chef_1.playerWalk.getKeyFrame(stateTime, true);
 		batch.begin();
-		batch.draw(chef_1_default, 400, 200);
+		if (chef_1.isStanding()){
+			chefFrame = chef_1.chefStanding;
+		}
+		chef_1.handleInput();
+		batch.draw(chefFrame, chef_1.getX(), chef_1.getY(), 50, 50);
 		batch.end();
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
 	}
 
 	@Override
