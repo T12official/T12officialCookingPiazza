5b53853eeb5ddc794daa71193833b570c575b685
harry-erskine <104079335+harry-erskine@users.noreply.github.com>
2023-01-20 12:26:11 +0000

    updated customer java file

--- core/src/com/mygdx/game/Customer.java
+++ core/src/com/mygdx/game/Customer.java
@@ -13,15 +13,25 @@ public class Customer {
 	public boolean recieveOrder(Dish dishReceived) {
 		
 		if ( dishesEqual(order, dishReceived) ) {
-			return true; //returns true when correct order is passed through
+			return true; // returns true when correct order is passed through
 		}
 		
-		return false; //returns false if the dish received is not what the customer ordered
+		return false; // returns false if the dish received is not what the customer ordered
 	}
 	
-	private boolean dishesEqual(Dish dish1, Dish dish2){
-		
-		//will update this function later
+	private boolean dishesEqual(Dish dish1, Dish dish2) {
+		// dish 1 is the dish the customer ordered
+		String[] ingredients1 = dish1.getIngredients();
+		// dish 2 is the dish the chef has handeded in
+		String[] ingredients2 = dish2.getIngredients();
+
+		// need to search through all the ingredients to make sure each dish has the same ingredients
+		int size = ingredients2.length;
+		for ( int i = 0; i < size; i++) {
+			// add code
+		}
+
+		// will update this function later
 		return false;
 	}
 }
