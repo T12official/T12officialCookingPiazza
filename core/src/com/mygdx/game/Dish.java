1a9fc9512398647a26c4c6edac750a0bc947e14d
NeloBlivion <41271523+neloblivion@users.noreply.github.com>
2023-01-25 18:06:34 +0000

    Add placeholder getIngredients function to fix current build

--- core/src/com/mygdx/game/Dish.java
+++ core/src/com/mygdx/game/Dish.java
@@ -1,6 +1,9 @@
 package com.mygdx.game;
-import java.util.*;
 
 public class Dish extends Ingredient {
-	
+	public String[] getIngredients() {
+        // returns an array of ingredients
+        // placeholder because broken customer?
+        return new String[] {"ingredient1", "ingredient2", "ingredient3"};
+    }
 }
