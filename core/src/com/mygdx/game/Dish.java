0cc6aba335d1c6a0be2f541892ae9261289b06a0
harry-erskine <104079335+harry-erskine@users.noreply.github.com>
2023-01-27 12:31:28 +0000

    Added pre-made recipe for burger and salad in Dish

--- core/src/com/mygdx/game/Dish.java
+++ core/src/com/mygdx/game/Dish.java
@@ -7,7 +7,32 @@ import java.util.List;
 public class Dish extends Ingredient {
 	private List<String> ingredients = new ArrayList<String>();
 
-	public Dish() {
+	public Dish(String dishName) {
+
+		// Burger and Salad are pre-made dishes for the customers to order
+		if (dishName == "Burger") {
+			this.addIngredients("cooked burger");
+			this.addIngredients("chopped tomato");
+			this.addIngredients("chopped bun");
+			this.addIngredients("plate");
+		}
+
+		else if (dishName == "Salad") {
+			this.addIngredients("chopped tomato");
+			this.addIngredients("chopped lettuce");
+			this.addIngredients("plate");
+		}
+
+		// Ingredients being put together in a dish will have to be put together automatically
+		else if (dishName == "new dish") {
+			// do nothing here
+			// this is for when any combination of ingredient are put together by the chef
+		}
+
+		else {
+			// pretend an error message pops up
+		}
+
 	}
 
 	public void addIngredients(String ingredientName) {
